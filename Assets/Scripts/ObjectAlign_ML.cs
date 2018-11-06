1161b164b4aee7e9d7375bbe99dc4bd4587f7f66
Mattias Lambert <mattiasl380@gmail.com>
2018-11-05 20:31:52 -0500

    Ball Physics Proto

---
+++ Assets/Scripts/ObjectAlign_ML.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectAlign_ML : MonoBehaviour
+{
+
+	void OnCollisionEnter(Collision coll)
+	{
+		Vector3 collPoint;
+		GameObject other = coll.gameObject;
+		
+		if (other.CompareTag("Player"))
+		{
+			collPoint = coll.contacts[0].point;
+			GameObject hitPoint = new GameObject("Contact Point");
+			hitPoint.transform.position = collPoint;
+			hitPoint.transform.parent = other.transform;
+			transform.parent = hitPoint.transform;
+			transform.position = transform.parent.position;
+		}
+	}
+}
